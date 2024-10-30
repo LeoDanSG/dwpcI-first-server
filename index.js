bbd0bae5561bfa2c5608e5965918da0280394d95
LeoDanSG <160193475+LeoDanSG@users.noreply.github.com>
2024-10-30 06:06:06 +0000

    ⭐Flujo de datos Metodos de envio de datos

--- index.js
+++ index.js
@@ -11,8 +11,66 @@ const server = http.createServer(async (req, res) => {
   console.log(`📣 CLIENT-REQUEST: ${req.url} ${req.method}`);
 
   // Enrutando peticiones
-  switch (url) {
-    case '/':
+switch (url) {
+  case '/':
+    // Peticion raiz
+    // Estableciendo cabeceras
+    res.setHeader('Content-Type', 'text/html');
+    // Escribiendo la respuesta
+    res.write(`
+    <html>
+      <head>
+        <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
+        <title>My App</title>
+      </head>
+      <body> 
+        <h1 style="color: #333">Hello from my server</h1>
+        <p style="color: #34495E">Estas en el recurso raiz.</p>
+      </body>
+    </html>
+    `);
+    console.log(`📣 Respondiendo: 200 ${req.url} ${req.method}`);
+    // Estableciendo codigo de respuesta
+    res.statusCode = 200;
+    // Cerrando la comunicacion
+    res.end();
+    break;
+  case '/author':
+    // Peticion raiz
+    // Estableciendo cabeceras
+    res.setHeader('Content-Type', 'text/html');
+    let url_image = 'https://media.istockphoto.com/id/180841365/photo/hes-a-handsome-man.jpg?s=612x612&w=0&k=20&c=vjQLLI8g_a0O6_xx0plUu3CJ9AMhnSzHssLwgem8gE4=';
+    // Escribiendo la respuesta
+    res.write(`
+    <html>
+      <head>
+        <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
+        <title>My App</title>
+      </head>
+      <body style="text-align: center;">
+        <h1 style="color: #333;">&#9889; Author &#9889;</h1>
+        <p style="color: #34495E;">Sosa Garcia Leonardo Daniel - Web Developer</p>
+        <div>
+          <img width="300px" src="https://media.istockphoto.com/id/180841365/photo/hes-a-handsome-man.jpg?s=612x612&w=0&k=20&c=vjQLLI8g_a0O6_xx0plUu3CJ9AMhnSzHssLwgem8gE4=" alt="Foto Ivan Rivalcoba">
+        </div>
+      </body>
+    </html>
+    `);
+    console.log(`📣 Respondiendo: 200 ${req.url} ${req.method}`);
+    // Estableciendo codigo de respuesta
+    res.statusCode = 200;
+    // Cerrando la comunicacion
+    res.end();
+    break;
+  case "/favicon.ico":
+    // Especificar la ubicación del archivo de icono
+    const faviconPath = path.join(__dirname, 'favicon.ico');
+    try {
+      const data = await fs.readFile(faviconPath);
+      res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+      res.end(data);
+    } catch (err) {
+      console.error(err);
       // Peticion raiz
       // Estableciendo cabeceras
       res.setHeader('Content-Type', 'text/html');
@@ -24,88 +82,63 @@ const server = http.createServer(async (req, res) => {
           <title>My App</title>
         </head>
         <body> 
-          <h1 style="color: #333">Hello from my server</h1>
-          <p style="color: #34495E">Estas en el recurso raiz.</p>
+          <h1>&#128534; 500 El server esta fuera de servicio</h1>
+          <p>Lo sentimos pero hubo un error en nuestro server...</p>
+          <p> ${err.message}</p>
         </body>
       </html>
       `);
-      console.log(`📣 Respondiendo: 200 ${req.url} ${req.method}`);
+      console.log(`📣 Respondiendo: 500 ${req.url} ${req.method}`);
       // Estableciendo codigo de respuesta
-      res.statusCode = 200;
+      res.statusCode = 500;
       // Cerrando la comunicacion
       res.end();
-      break;
-    case '/author':
-      // Peticion raiz
-      // Estableciendo cabeceras
-      res.setHeader('Content-Type', 'text/html');
-      let url_image = 'https://media.istockphoto.com/id/180841365/photo/hes-a-handsome-man.jpg?s=612x612&w=0&k=20&c=vjQLLI8g_a0O6_xx0plUu3CJ9AMhnSzHssLwgem8gE4=';
-      // Escribiendo la respuesta
-      res.write(`
-      <html>
-        <head>
-          <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
-          <title>My App</title>
-        </head>
-        <body style="text-align: center;">
-          <h1 style="color: #333;">&#9889; Author &#9889;</h1>
-          <p style="color: #34495E;">Sosa Garcia Leonardo Daniel - Web Developer</p>
-          <div>
-            <img width="300px" src="https://media.istockphoto.com/id/180841365/photo/hes-a-handsome-man.jpg?s=612x612&w=0&k=20&c=vjQLLI8g_a0O6_xx0plUu3CJ9AMhnSzHssLwgem8gE4=" alt="Foto Ivan Rivalcoba">
-          </div>
-        </body>
-      </html>
-      `);
-      console.log(`📣 Respondiendo: 200 ${req.url} ${req.method}`);
-      // Estableciendo codigo de respuesta
-      res.statusCode = 200;
-      // Cerrando la comunicacion
-      res.end();
-      break;
-    case "/favicon.ico":
-      // Especificar la ubicación del archivo de icono
-      const faviconPath = path.join(__dirname, 'favicon.ico');
-      try {
-        const data = await fs.readFile(faviconPath);
-        res.writeHead(200, { 'Content-Type': 'image/x-icon' });
-        res.end(data);
-      } catch (err) {
-        console.error(err);
-        // Peticion raiz
-        // Estableciendo cabeceras
-        res.setHeader('Content-Type', 'text/html');
-        // Escribiendo la respuesta
-        res.write(`
-        <html>
-          <head>
-            <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
-            <title>My App</title>
-          </head>
-          <body> 
-            <h1>&#128534; 500 El server esta fuera de servicio</h1>
-            <p>Lo sentimos pero hubo un error en nuestro server...</p>
-            <p> ${err.message}</p>
-          </body>
-        </html>
-        `);
-        console.log(`📣 Respondiendo: 500 ${req.url} ${req.method}`);
-        // Estableciendo codigo de respuesta
-        res.statusCode = 500;
-        // Cerrando la comunicacion
-        res.end();
-      }
-      break;
-    case "/message":
-      // Verificando si es post
-      if (method === "POST") {
+    }
+    break;
+  case "/message":
+    // Verificando si es post
+    if (method === "POST") {
+      // Se crea una variable para almacenar los
+      // Datos entrantes del cliente
+      let body = "";
+      // Se registra un manejador de eventos
+      // Para la recepción de datos
+      req.on("data", (data => {
+        body += data;
+        if (body.length > 1e6) return req.socket.destroy();
+      }));
+      // Se registra una manejador de eventos
+      // para el termino de recepción de datos
+      req.on("end", () => {
         // Procesa el formulario
         res.statusCode = 200;
-        res.write("🎉 Endpoint Funcionando!!! 🎉");
+        res.setHeader("Content-Type", "text/html");
+         // Mediante URLSearchParams se extraen
+			    // los campos del formulario
+          const params = new URLSearchParams(body);
+          // Se construye un objeto a partir de los datos
+			    // en la variable params
+          const parsedParams = Object.fromEntries(params);
+          res.write(`
+            <html>
+              <head>
+                <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
+                <title>My App</title>
+              </head>
+              <body> 
+                <h1 style="color: #333">SERVER MESSAGE RECIEVED &#128172</h1>
+                <p>${parsedParams.message}</p>
+              </body>
+            </html>
+            `);
+             // Se finaliza la conexion
+          return res.end();
+        })
       } else {
         res.statusCode = 404;
         res.write("404: Endpoint no encontrado")
+        res.end();
       }
-      res.end();
       break;
       // Continua con el defautl
     default:
@@ -135,5 +168,5 @@ const server = http.createServer(async (req, res) => {
 }); 
 
 server.listen(8080, "0.0.0.0", () => {
-  console.log("👩‍🍳 Servidor escuchando en http://localhost:8080"); 
+  console.log("👩‍🍳 Servidor escuchando en http://localhost:8080");
 });
